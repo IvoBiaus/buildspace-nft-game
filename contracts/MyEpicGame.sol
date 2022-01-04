5cf56fcc3fe7f52201789f4cf714c04ba12408bf
Ivo Biaus <ivobiaus@gmail.com>
2022-01-04 20:50:16 -0300

    Setup data for NFTs

--- contracts/MyEpicGame.sol
+++ contracts/MyEpicGame.sol
@@ -1,11 +1,46 @@
-// SPDX-License-Identifier: UNLICENSED
-
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
 contract MyEpicGame {
-    constructor() {
-        console.log("THIS IS MY GAME CONTRACT. NICE.");
+    struct CharacterAttributes {
+        uint256 characterIndex;
+        string name;
+        string imageURI;
+        uint256 hp;
+        uint256 maxHp;
+        uint256 attackDamage;
+    }
+
+    CharacterAttributes[] defaultCharacters;
+
+    constructor(
+        string[] memory characterNames,
+        string[] memory characterImageURIs,
+        uint256[] memory characterHp,
+        uint256[] memory characterAttackDmg
+    ) {
+        // Loop through the characters and save them in the contract
+        for (uint256 i = 0; i < characterNames.length; i += 1) {
+            defaultCharacters.push(
+                CharacterAttributes({
+                    characterIndex: i,
+                    name: characterNames[i],
+                    imageURI: characterImageURIs[i],
+                    hp: characterHp[i],
+                    maxHp: characterHp[i],
+                    attackDamage: characterAttackDmg[i]
+                })
+            );
+
+            CharacterAttributes memory c = defaultCharacters[i];
+            console.log(
+                "Done initializing %s w/ HP %s, img %s",
+                c.name,
+                c.hp,
+                c.imageURI
+            );
+        }
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,6 +1,16 @@
 const main = async () => {
   const gameContractFactory = await hre.ethers.getContractFactory("MyEpicGame");
-  const gameContract = await gameContractFactory.deploy();
+  const gameContract = await gameContractFactory.deploy(
+    ["Volcanic Titan", "Swamp Monster", "Forest Guardian", "Sea Monster"],
+    [
+      "https://i.imgur.com/bl8Wb04.jpeg",
+      "https://i.imgur.com/LP36sBj.jpeg",
+      "https://i.imgur.com/NUKZCSs.jpeg",
+      "https://i.imgur.com/hWGeUua.jpeg",
+    ],
+    [1000, 300, 500, 800], // HP
+    [500, 200, 200, 400] // Attack damage
+  );
   await gameContract.deployed();
   console.log("Contract deployed to:", gameContract.address);
 };
