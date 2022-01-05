4a4572ca8b3339158648fe9fc7e79912b01956de
Ivo Biaus <ivobiaus@gmail.com>
2022-01-04 21:40:11 -0300

    Minting NFT. Setup tokenURI.

--- contracts/MyEpicGame.sol
+++ contracts/MyEpicGame.sol
@@ -1,9 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
 import "hardhat/console.sol";
 
-contract MyEpicGame {
+import "./libraries/Base64.sol";
+
+// Contract inherits from ERC721 (standard NFT contract)
+contract MyEpicGame is ERC721 {
     struct CharacterAttributes {
         uint256 characterIndex;
         string name;
@@ -13,14 +20,23 @@ contract MyEpicGame {
         uint256 attackDamage;
     }
 
+    // The tokenId is the NFTs unique identifier
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
     CharacterAttributes[] defaultCharacters;
+
+    // tokenId => NFTs attributes
+    mapping(uint256 => CharacterAttributes) public nftHolderAttributes;
+    // address => NFTs tokenId
+    mapping(address => uint256) public nftHolders;
 
     constructor(
         string[] memory characterNames,
         string[] memory characterImageURIs,
         uint256[] memory characterHp,
         uint256[] memory characterAttackDmg
-    ) {
+    ) ERC721("Creatures", "CRTR") {
         // Loop through the characters and save them in the contract
         for (uint256 i = 0; i < characterNames.length; i += 1) {
             defaultCharacters.push(
@@ -35,6 +51,7 @@ contract MyEpicGame {
             );
 
             CharacterAttributes memory c = defaultCharacters[i];
+
             console.log(
                 "Done initializing %s w/ HP %s, img %s",
                 c.name,
@@ -42,5 +59,78 @@ contract MyEpicGame {
                 c.imageURI
             );
         }
+
+        // Increment tokenIds
+        _tokenIds.increment();
+    }
+
+    function mintCharacterNFT(uint256 _characterIndex) external {
+        uint256 newItemId = _tokenIds.current();
+
+        // Assigns the tokenId to the caller's wallet address
+        _safeMint(msg.sender, newItemId);
+
+        // Map the tokenId => character attributes
+        nftHolderAttributes[newItemId] = CharacterAttributes({
+            characterIndex: _characterIndex,
+            name: defaultCharacters[_characterIndex].name,
+            imageURI: defaultCharacters[_characterIndex].imageURI,
+            hp: defaultCharacters[_characterIndex].hp,
+            maxHp: defaultCharacters[_characterIndex].maxHp,
+            attackDamage: defaultCharacters[_characterIndex].attackDamage
+        });
+
+        console.log(
+            "Minted NFT w/ tokenId %s and characterIndex %s",
+            newItemId,
+            _characterIndex
+        );
+
+        // Easy way to see who owns what NFT.
+        nftHolders[msg.sender] = newItemId;
+
+        // Increment the tokenId for the next person that uses it.
+        _tokenIds.increment();
+    }
+
+    function tokenURI(uint256 _tokenId)
+        public
+        view
+        override
+        returns (string memory)
+    {
+        CharacterAttributes memory charAttributes = nftHolderAttributes[
+            _tokenId
+        ];
+
+        string memory strHp = Strings.toString(charAttributes.hp);
+        string memory strMaxHp = Strings.toString(charAttributes.maxHp);
+        string memory strAttackDamage = Strings.toString(
+            charAttributes.attackDamage
+        );
+
+        string memory json = Base64.encode(
+            abi.encodePacked(
+                '{"name": "',
+                charAttributes.name,
+                " -- NFT #: ",
+                Strings.toString(_tokenId),
+                '", "description": "This is an NFT that lets people play in the game Metaverse Slayer!", "image": "',
+                charAttributes.imageURI,
+                '", "attributes": [ { "trait_type": "Health Points", "value": ',
+                strHp,
+                ', "max_value":',
+                strMaxHp,
+                '}, { "trait_type": "Attack Damage", "value": ',
+                strAttackDamage,
+                "} ]}"
+            )
+        );
+
+        string memory output = string(
+            abi.encodePacked("data:application/json;base64,", json)
+        );
+
+        return output;
     }
 }
